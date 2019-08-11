c4be203bc3a3d6ce118de7260bd74cd0aab59d47
Pragin <pragin.martin@hotmail.com>
2019-08-11 10:24:22 +0100

    Completed User API rotues

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect to Database
 connectDB();
 
+//Init Middleware
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('API Running...'));
 
 //Define Routes
