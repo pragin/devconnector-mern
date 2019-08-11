c3b6f2e875376d5d7d7f25cafd4f871cafcb9b9d
Pragin <pragin.martin@hotmail.com>
2019-08-11 12:49:37 +0100

    Custom auth middleware & JWT verification

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 /**
  *  @route GET api/auth
  *  @desc Test route
  *  @access Public
  */
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
