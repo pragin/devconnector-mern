e818dddefb2effd61326ebf6faf8e8cc9c7cb07a
Pragin <pragin.martin@hotmail.com>
2019-09-10 20:26:37 +0100

    registration form with useState hook

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+const App = () => (
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/Login" component={Login} />
+        </Switch>
+      </section>
+    </>
+  </Router>
+);
+
+export default App;
